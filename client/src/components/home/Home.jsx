719ce1590d4f1d6eb9c69590f54b2508ff93a635
JCool221 <109809337+JCool221@users.noreply.github.com>
2023-07-30 04:27:34 -0400

    added photo

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,10 +1,11 @@
 import './home.css'
+import me from '../../assets/me.jpg'
 
 function Home() {
   return (
     <div className='homewrap'>
     <div className='homepage'>
-      <img src="https://fakeimg.pl/250x350/?text=Hello" />
+      <img className='home-img' src={me} />
       <article className='hometext'>Hello there, my name is Josh and I like to make web apps.  I write clean, readable code with a focus on accessibility and maintainabilty.  I  know my way around several frameworks as well as no framework javascript but I really like to work with React.  I like to build attractive, accessible, responsive components.<br/>  Anyway, I look forward to hearing from you!
       </article>
     </div>
